9e7ce6a544b0f8170888f7396b23d1768cc6b7bb
linyuan1105 <673609957@qq.com>
2022-07-30 07:54:17 +0800

    feature: ts前述

--- src/ts.ts
+++ src/ts.ts
@@ -1,14 +1,5 @@
-// 属性联合
-type Combined = { a: number } & { b: string };
-// 例如
-const t:Combined = {
-    a:0,
-    b:'9'
-}
-
-
-type Conflicting = { a: number } & { a: string };
-const o: Conflicting = {
-    a:'',
-    a:9
-}
+const user = {
+    name: "Daniel",
+    age: 26,
+};
+user.location = 0
